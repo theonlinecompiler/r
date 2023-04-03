bd9fb16eddb1fea975e5a6d6d19f595337bdc3b5
theonlinecompiler <129802136+theonlinecompiler@users.noreply.github.com>
2023-04-04 04:04:11 +0530

    Delete 7_2.R

--- 7_2.R
+++
@@ -1,4 +0,0 @@
-Height =c(60,62,64,66,68,70,72,74)
-Weight =c(92,83,101,110,128,119,137,146)
-cor(Height,Weight, method = "spearman")
-ggplot()+aes(x=Height,y=Weight)+geom_line(col="blue")+theme_minimal()
